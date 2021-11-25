7f722d96dc0d47116cc6ed780a7d42ed9c2dd266
Rinat Namazov <rinat.namazov@rinwares.com>
2021-11-25 12:20:41 +0300

    Added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as thisPackage from '../package.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   app.enableShutdownHooks();
+
+  const config = new DocumentBuilder()
+    .setTitle(thisPackage.name)
+    .setDescription(thisPackage.description)
+    .setVersion(thisPackage.version)
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
